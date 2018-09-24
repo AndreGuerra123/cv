204cfa06b5cb82bc66dde739ac8b192d28acdf02
AndreGuerra123 <guerraandre@hotmail.com>
2018-09-24 11:46:40 +0100

    remove unnecessary stuff

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let server = express();
 server.use('/', express.static(__dirname));
-server.on('connection',()=>{console.log("C.V. on port 8080.")})
 server.listen(8080);
 
 
