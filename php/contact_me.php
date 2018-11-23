899be3314f13bf51b86c10dc3fcb5fc8e8b74d51
AndreGuerra123 <guerraandre@hotmail.com>
2018-11-23 20:58:45 +0000

    asjdjdada

--- php/contact_me.php
+++ php/contact_me.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Demo contact form <demo@domain.com>';
+$from = 'Curriculum Vitae Form';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = getenv("EMAIL")
 
 // subject of the email
 $subject = 'New message from contact form';
